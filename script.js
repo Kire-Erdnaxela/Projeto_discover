8858cd9222c0864e03fa142aa3ed99014892a602
“Erik” <erik.alexandre147@gmail.com>
2024-04-08 21:44:35 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleMode(){
+  const html = document.documentElement
+  html.classList.toggle('light')
+
+  const img = document.querySelector("#profile img")
+
+  if(html.classList.contains('light')){
+    img.setAttribute('src', './assets/avatar-light.png')
+  }else {
+    img.setAttribute("src", "./assets/Avatar.png")
+  } 
+  if (html.classList.contains("light")) {
+    img.setAttribute("alt", "Quem usa tema claro?")
+  } else {
+    img.setAttribute("alt", "imagem de perfil de um guaxinim fofo")
+  } 
+ }
+   
